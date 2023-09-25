ad1cf391223f449fbc346d4bdffd4e0f3016e283
Joseph Abayomi <josephabayomi00@gmail.com>
2023-09-25 01:08:51 +0100

    the nav page now toggles on one click
    i made the nave to toggle at once by adding `" || navItem[i].style.display === ""` to the nav.js function.
    and i removed the active class from the navFooter.css

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ const navItem = document.getElementsByClassName("nav-item");
 
 function toggle() {
     for (let i = 0; i < navItem.length; i++) {
-        if (navItem[i].style.display === "none") {
+        if (navItem[i].style.display === "none" || navItem[i].style.display === "") {
             navItem[i].style.display = "block";
         } else {
             navItem[i].style.display = "none";
