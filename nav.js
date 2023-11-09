982fdfefcf83f910ef00803be81d0a38155fc0d9
Joseph Abayomi <josephabayomi00@gmail.com>
2023-11-09 20:44:46 +0100

    I was able to make the toggleItem function work :)

--- nav.js
+++ nav.js
@@ -1,11 +1,8 @@
 const navItem = document.getElementsByClassName("nav-item");
 
-function toggle() {
+function toggleNavItems() {
     for (let i = 0; i < navItem.length; i++) {
-        if (navItem[i].style.display === "none" || navItem[i].style.display === "") {
-            navItem[i].style.display = "block";
-        } else {
-            navItem[i].style.display = "none";
-        }
+        navItem[i].classList.toggle("hidden");
     }
 }
+
